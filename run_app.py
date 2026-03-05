937dc611f96a2495a59b52caae3ce2f466ae0773
Dmitry <ramses@bk.ru>
2026-03-05 21:42:27 +0300

    Update run_app.py
    Отключен запрос email при запуске Streamlit

--- run_app.py
+++ run_app.py
@@ -6,13 +6,16 @@ def main():
     # Предохранитель для Windows
     multiprocessing.freeze_support()
 
+    # Отключаем запрос email и сбор статистики Streamlit
+    os.environ["STREAMLIT_GATHER_USAGE_STATS"] = "false"
+
     # Переход в папку с распакованными файлами .exe
     if hasattr(sys, '_MEIPASS'):
         os.chdir(sys._MEIPASS)
 
     import streamlit.web.cli as stcli
     
-    # Запускаем Streamlit с явным отключением режима разработчика
+    # Запускаем Streamlit
     sys.argv = [
         "streamlit", 
         "run", 
