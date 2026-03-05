922ee19a70c39a9b56a4b3cad3fb4a0701b45085
Dmitry <ramses@bk.ru>
2026-03-05 21:30:24 +0300

    Update run_app.py
    Отключение developmentMode для Streamlit

--- run_app.py
+++ run_app.py
@@ -3,18 +3,24 @@ import sys
 import multiprocessing
 
 def main():
-    # 1. Предохранитель для Windows: останавливает бесконечный запуск процессов
+    # Предохранитель для Windows
     multiprocessing.freeze_support()
 
-    # 2. Указываем программе искать dashboard.py внутри распакованного .exe
+    # Переход в папку с распакованными файлами .exe
     if hasattr(sys, '_MEIPASS'):
         os.chdir(sys._MEIPASS)
 
-    # 3. Запускаем Streamlit напрямую через его внутренний модуль
     import streamlit.web.cli as stcli
     
-    # Имитируем команду "streamlit run dashboard.py"
-    sys.argv = ["streamlit", "run", "dashboard.py", "--server.port=8501", "--server.headless=false"]
+    # Запускаем Streamlit с явным отключением режима разработчика
+    sys.argv = [
+        "streamlit", 
+        "run", 
+        "dashboard.py", 
+        "--global.developmentMode=false", 
+        "--server.port=8501", 
+        "--server.headless=false"
+    ]
     sys.exit(stcli.main())
 
 if __name__ == '__main__':
